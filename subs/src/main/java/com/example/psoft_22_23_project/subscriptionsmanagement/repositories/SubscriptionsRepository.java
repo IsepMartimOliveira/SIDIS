c917ad483861a768b129b89afd7350f3d06c990e
IsepMartimOliveira <1181754@isep.ipp.pt>
2023-10-17 20:41:09 +0100

    SubscriptionRep

--- subs/src/main/java/com/example/psoft_22_23_project/subscriptionsmanagement/repositories/SubscriptionsRepository.java
+++ subs/src/main/java/com/example/psoft_22_23_project/subscriptionsmanagement/repositories/SubscriptionsRepository.java
@@ -2,34 +2,64 @@ package com.example.psoft_22_23_project.subscriptionsmanagement.repositories;
 
 
 import com.example.psoft_22_23_project.subscriptionsmanagement.model.Subscriptions;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import javax.validation.constraints.NotNull;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.Optional;
+
 @Repository
-public interface SubscriptionsRepository extends JpaRepository<Subscriptions, Long> {
-
-
+@Configuration
+@Primary
+public interface SubscriptionsRepository extends SubscriptionsRepoCustom,JpaRepository<Subscriptions, Long> {
     Optional<Subscriptions> findById(long id);
-
-/*
-    Optional<Subscriptions> findByPlan(Plans plans);
-
-    boolean existsByPlanAndUserNotNull(Plans plans);
-
-    @Query("SELECT s FROM Subscriptions s WHERE s.id = :id")
-    List<Subscriptions> searchById(@Param("id") Long id);
-
-    Optional<Subscriptions> findByUser(User user);
-
-
-    List<Subscriptions> findAllByPlanAndActiveStatus_Active(Plans plan, @NonNull boolean activeStatus_active);
-
-    Optional<Subscriptions> findByActiveStatus_ActiveAndUser(@NotNull boolean active, @NotNull User user);
-
- */
 
 }
 
+interface SubscriptionsRepoCustom {
+    HttpResponse<String> getPlansFromOtherAPI(String name) throws URISyntaxException, IOException, InterruptedException;
+
+}
+
+
+@RequiredArgsConstructor
+class SubscriptionsRepoCustomImpl implements SubscriptionsRepoCustom {
+    @Value("${server.port}")
+    private int currentPort;
+    @Override
+    public HttpResponse<String> getPlansFromOtherAPI(String name) throws URISyntaxException, IOException, InterruptedException {
+    //Falta implementar o endpoint
+        int otherPort = (currentPort == 8082) ? 8091 : 8082;
+        URI uri = new URI("http://localhost:" + otherPort + "/api/plans/" + name);
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(uri)
+                .GET()
+                .build();
+
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        return response;
+
+    }
+
+}
 
 
