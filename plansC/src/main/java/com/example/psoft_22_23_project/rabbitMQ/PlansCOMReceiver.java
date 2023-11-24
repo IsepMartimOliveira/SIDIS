159eabc3f2f9016b5d665318d787bccdbc995121
franciscoreis <1201373@isep.ipp.pt>
2023-11-24 18:47:40 +0000

    rabit

--- plansC/src/main/java/com/example/psoft_22_23_project/rabbitMQ/PlansCOMReceiver.java
+++ plansC/src/main/java/com/example/psoft_22_23_project/rabbitMQ/PlansCOMReceiver.java
@@ -1,14 +1,21 @@
 package com.example.psoft_22_23_project.rabbitMQ;
 
 import com.example.psoft_22_23_project.plansmanagement.api.CreatePlanRequest;
+import com.example.psoft_22_23_project.plansmanagement.model.Plans;
+import com.example.psoft_22_23_project.plansmanagement.services.CreatePlansMapperImpl;
+import com.example.psoft_22_23_project.plansmanagement.services.PlansServiceImpl;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+
 @Component
 public class PlansCOMReceiver {
-
+    PlansServiceImpl plansService;
     @RabbitListener(queues = "#{plansQueue.name}")
-    public void receivePlan(CreatePlanRequest planRequest) {
+    public void receivePlan(CreatePlanRequest planRequest) throws URISyntaxException, IOException, InterruptedException {
+        plansService.create(planRequest);
         System.out.println(" [x] Received '" + planRequest + "' from plans_queue");
     }
 }
